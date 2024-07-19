f6854468bcfa56e099a28a7ff2d10d8486bc9f5f
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-07-19 23:17:49 +0530

    intiger game added

---
+++ Intiger_Game.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define ll long long
+#define intl long long int
+#define ldbl long double
+
+void solve(){
+    int n;
+    cin>>n;
+    if(n%3==0){
+        cout<<"SECOND"<<endl;
+    }else{
+        cout<<"FIRST"<<endl;
+    }
+
+}
+
+int main() {
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
